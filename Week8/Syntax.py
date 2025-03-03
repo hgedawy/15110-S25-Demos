1129726d33feb23d170a08a58d1b34255df14e64
Hend Gedawy <hkg@qatar.win.cmu.edu>
2025-03-03 16:11:48 +0300

    wk8

--- Week8/Syntax.py
+++ Week8/Syntax.py
@@ -84,6 +84,9 @@ print("\n Local ########")
 numbers = [1, 3, 6, 2]
 x = average(numbers)
 print(x)
+#print(L)
+#print(avg)
+#print(n_elements)
 
 
 
@@ -106,7 +109,7 @@ print(x)
 
 ### Clean way to define global scoping
 def average_global():
-    ##
+    global L
     n_elements = len(L)
     avg = sum(L) / n_elements
     return avg
@@ -124,7 +127,7 @@ x = 10
 def f():
     global x
     y = x+1 # Reads x
-    ##
+    
     x= 3
     print("in f:", x)
     return y
@@ -141,7 +144,7 @@ def median():
 
 L = [1, 3, 6, 2, 0]
 m = median()
-#print(L)
+print(L)
 
 
 
@@ -152,16 +155,17 @@ def quadratic_roots(a, b, c):
     sqr = math.sqrt(b**2 - (4 * a *c)) 
     return (-b + sqr) * d, (-b - sqr) * d
 
-# print("\n Named Parameters #########################################")
+print("\n Named Parameters #########################################")
 
 # order matters
-# print(quadratic_roots(2, 5, 3))
-# print(quadratic_roots(3, 5, 2))
+print(quadratic_roots(2, 5, 3))
+print(quadratic_roots(3, 5, 2))
 
 
 ### how to do it in a way that would make it safe to change the order
 #Passing arguments as keyword (named) arguments
-
+print(quadratic_roots(a=2, b=5, c=3))
+print(quadratic_roots(c=3, b=5, a=2))
 
 
 
@@ -181,43 +185,49 @@ def f(a, b):
     c = a + b
     return c
 
-
-
+help(sum)
+help(f)
 
 ######################### Default Parameters #######
 
-def quadratic_roots_default():
-    return None
+def quadratic_roots_default(a = 1, b = -3, c = 2):
+    d = 1 / (2 * a)
+    sqr = math.sqrt(b**2 - (4 * a *c)) 
+    return (-b + sqr) * d, (-b - sqr) * d
     
     
-# print("\n Default Parameters #########################################")
+print("\n Default Parameters #########################################")
 
 #A default value can be defined for each argument
 
 
 #the parameter takes its default value, if no value passed
-#x1, x2 = quadratic_roots_default()
-#print("Default: ", x1, x2)
+x1, x2 = quadratic_roots_default() # quadratic_roots_default(a = 1, b = -3, c = 2)
+print("Default: ", x1, x2)
 
+x1, x2 = quadratic_roots_default(b= 2, c= -3, a= 1) 
+print("Default: ", x1, x2)
 
 #if a value is passed for a parameter with a default value, the parameters takes the value of the provided argument
 
 
 #### Equivelant Function Calls
 
-#print(quadratic_roots_default(b=2, c=-3) == quadratic_roots_default(??)) #??
-#print(quadratic_roots_default(b=4) == quadratic_roots_default(??))
+print(quadratic_roots_default(b=2, c=-3) == quadratic_roots_default(a=1, b=2, c=-3)) #??
+print(quadratic_roots_default(b=4) == quadratic_roots_default(a=1, b=4, c=2))
 
 
 
 ####################### Be careful mixing up positional and named parameters! 
-# print("\n XX Mixing Positional and Named Arguments XX #########################################")
+print("\n XX Mixing Positional and Named Arguments XX #########################################")
 
-#1, x2 = quadratic_roots_default(2, c=-3)
-#x3, x4= quadratic_roots_default(??)
-#print(x1==x3 and x2 == x4)
+x1, x2 = quadratic_roots_default(2, c=-3)
+x3, x4= quadratic_roots_default(a=2, b=-3, c=-3)
+print(x1==x3 and x2 == x4)
 
 #issue 1
+x1, x2 = quadratic_roots_default(2, a=-3)
 
 
 #issue 2
+#x3, x4= quadratic_roots_default(c=-3, 2, -3)
